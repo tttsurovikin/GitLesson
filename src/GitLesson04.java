27d6169af5541cd797146a3939843319dde7c0c3
tttsurovikin <stimofey03@gmail.com>
2023-10-15 10:14:45 +0200

    add system out println

--- src/GitLesson04.java
+++ src/GitLesson04.java
@@ -1,6 +1,7 @@
 public class GitLesson04 {
     public static void main(String [] args){
         System.out.println("Hello world and Git I am Tim ");
+        System.out.println("Second commit for Git");
 
     }
 }
